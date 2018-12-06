753a2b1c8e4dcb238d8866aa31ac00dc3b6241c6
root <mark.hakansson@live.se>
2018-12-06 09:59:07 +0000

    Changes to account

--- account/terminateAccount.php
+++ account/terminateAccount.php
@@ -1,43 +1,46 @@
 <?php
 	session_start();
     header('Content-Type: text/html; charset=ISO-8859-1');
-	include '../header.php'
+	include '../connect.php';
+	include '../header.php';
+
+	$password = $passwordErr = "";
+	
+	if($_SERVER["REQUEST_METHOD"] == "POST" and isset($_POST["userId"])) {
+		if(!empty($_POST["password"])) {
+			$password = $_POST["password"];
+		} else {
+			$passwordErr = "Password field is empty!";
+		}
+	
+		// Should only delete from users and reviews
+		// because we might need information on orders
+		if(!empty($passwordErr)) {
+			
+		}	
+	}
+
+ 	
+
 ?>
 
-<!DOCTYPE html>
-<html lang="sv">
-<head>
-</head>
-<body>
+
 <div class="terminate_container">		
 	<a href = "index.php">Go to home</a>
 	<h2>Account termination</h2>
 	<p>If you want to terminate your account, please input your password.
-	   Remember this will <b>DELETE ALL</b> your personal information from this website. </p>
+	   Remember this will <b>DELETE ALL</b> your personal information from this website. Data on
+	   purchases and orders will be kept for 3 years from date of purcase. This is due to the product warranty we give you.</p>
 	<form method = "post">
     <div>
 		<label for="password">Password:</label>
-    	<input type="password" name="password">
+		<input type="password" name="password">
    	</div>
 
 	<div>
         <input type="submit" name="submit" value="Terminate account">
-        <span class="required">
-		</span>
+		<?php echo "$passwordErr";?>
     </div>
 
 	</form>
 </div>
-
-<?php 
-	if($_SERVER["REQUEST_METHOD"] == "POST") {
-		if(!empty($_POST["password"])) {
-			echo "Goodday to you";
-		} else {
-			echo "Didn't work :(";
-		}
-	} 
-?>
-
-</body>
-</html>
